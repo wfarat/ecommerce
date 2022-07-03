bdda32158c35ec33d33bcf48086cf53125c6f481
wfarat <wfarat@gmail.com>
2022-07-03 16:48:02 +0200

    Created react-app for view, added files for structure

---
+++ view/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './index.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+        <Route path="/" element={<App />}></Route>
+        </Routes>
+      </Router>
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
